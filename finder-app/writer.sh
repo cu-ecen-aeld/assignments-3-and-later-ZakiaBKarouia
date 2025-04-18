87506609654c6cdde28ee510a378e65e1023935a
Zakia Ben Karouia <zbenkarouia@gmail.com>
2025-04-18 23:53:56 +0200

    assignement 1

---
+++ finder-app/writer.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+if [ $# -ne 2 ]; then
+  echo "Error: Missing arguments."
+  echo "Usage: $0 <writefile> <writestr>"
+  exit 1
+fi
+
+writefile=$1
+writestr=$2
+
+dirpath=$(dirname "$writefile")
+
+mkdir -p "$dirpath"
+
+if [ $? -ne 0 ]; then
+  echo "Error: Failed to create directory '$dirpath'"
+  exit 1
+fi
+
+echo "$writestr" > "$writefile"
+if [ $? -ne 0 ]; then
+  echo "Error: Failed to write to file '$writefile'"
+  exit 1
+fi
+
+exit 0
--- student-test/assignment1/Test_validate_username.c
+++ student-test/assignment1/Test_validate_username.c
@@ -18,5 +18,9 @@ void test_validate_my_username()
      * TODO: Replace the line below with your code here as described above to verify your /conf/username.txt 
      * config file and my_username() functions are setup properly
      */
-    TEST_ASSERT_TRUE_MESSAGE(false,"AESD students, please fix me!");
+    const char* expected_name = my_username();
+    const char* actual_name = malloc_username_from_conf_file() ;
+    TEST_ASSERT_EQUAL_STRING_MESSAGE(expected_name,actual_name,"NOT A MATCH");
+    
+    free((void*)actual_name);
 }
