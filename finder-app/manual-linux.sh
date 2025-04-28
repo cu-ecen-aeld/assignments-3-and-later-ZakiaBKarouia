24ba290a866ba462a4eda440f16fa780fa57a5c0
Zakia Ben Karouia <zbenkarouia@gmail.com>
2025-04-28 18:54:24 +0200

    assignment 3 part 2

--- finder-app/manual-linux.sh
+++ finder-app/manual-linux.sh
@@ -15,29 +15,43 @@ CROSS_COMPILE=aarch64-none-linux-gnu-
 
 if [ $# -lt 1 ]
 then
+    OUTDIR="/tmp/aeld"
 	echo "Using default directory ${OUTDIR} for output"
 else
-	OUTDIR=$1
+	OUTDIR=$(realpath $1)
 	echo "Using passed directory ${OUTDIR} for output"
 fi
 
-mkdir -p ${OUTDIR}
-
+if [ ! -d "$OUTDIR" ]; then
+    mkdir -p "$OUTDIR"
+    if [ $? -ne 0 ]; then
+        echo "ERROR: Failed to create output directory $OUTDIR"
+        exit 1
+    fi
+fi
 cd "$OUTDIR"
+echo ${OUTDIR}
 if [ ! -d "${OUTDIR}/linux-stable" ]; then
     #Clone only if the repository does not exist.
 	echo "CLONING GIT LINUX STABLE VERSION ${KERNEL_VERSION} IN ${OUTDIR}"
-	git clone ${KERNEL_REPO} --depth 1 --single-branch --branch ${KERNEL_VERSION}
+    git clone --depth 1 --branch ${KERNEL_VERSION} ${KERNEL_REPO} linux-stable
 fi
 if [ ! -e ${OUTDIR}/linux-stable/arch/${ARCH}/boot/Image ]; then
     cd linux-stable
     echo "Checking out version ${KERNEL_VERSION}"
     git checkout ${KERNEL_VERSION}
 
-    # TODO: Add your kernel build steps here
+    echo "Building kernel..."
+    make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} mrproper
+    make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} defconfig
+    make -j$(nproc) ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} all
+    make -j$(nproc) ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} modules
+    make -j$(nproc) ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} dtbs
 fi
 
 echo "Adding the Image in outdir"
+
+cp ${OUTDIR}/linux-stable/arch/arm64/boot/Image $OUTDIR
 
 echo "Creating the staging directory for the root filesystem"
 cd "$OUTDIR"
@@ -46,8 +60,12 @@ then
 	echo "Deleting rootfs directory at ${OUTDIR}/rootfs and starting over"
     sudo rm  -rf ${OUTDIR}/rootfs
 fi
-
-# TODO: Create necessary base directories
+mkdir -p ${OUTDIR}/rootfs
+cd ${OUTDIR}/rootfs
+mkdir -p bin dev etc home lib proc sbin sys tmp usr var
+mkdir -p usr/bin usr/lib usr/sbin
+mkdir -p var/log
+mkdir -p home/conf
 
 cd "$OUTDIR"
 if [ ! -d "${OUTDIR}/busybox" ]
@@ -56,25 +74,55 @@ git clone git://busybox.net/busybox.git
     cd busybox
     git checkout ${BUSYBOX_VERSION}
     # TODO:  Configure busybox
+    make distclean
+    make defconfig
 else
     cd busybox
 fi
 
 # TODO: Make and install busybox
+make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} CONFIG_PREFIX=${OUTDIR}/rootfs install
 
 echo "Library dependencies"
-${CROSS_COMPILE}readelf -a bin/busybox | grep "program interpreter"
-${CROSS_COMPILE}readelf -a bin/busybox | grep "Shared library"
+echo ${PWD}
+${CROSS_COMPILE}readelf -a ${OUTDIR}/rootfs/bin/busybox | grep "program interpreter"
+${CROSS_COMPILE}readelf -a ${OUTDIR}/rootfs/bin/busybox | grep "Shared library"
 
 # TODO: Add library dependencies to rootfs
+SYSROOT="/home/zakia/GNU/arm-gnu-toolchain-13.3.rel1-x86_64-aarch64-none-linux-gnu/aarch64-none-linux-gnu/libc"
+cp  ${SYSROOT}/lib/ld-linux-aarch64.so.1 ${OUTDIR}/rootfs/lib/
+#cp  ${SYSROOT}/lib64/ld-*.so* ${OUTDIR}/rootfs/lib64/
+mkdir -p ${OUTDIR}/rootfs/lib64/
+cp  ${SYSROOT}/lib64/libc*.so* ${OUTDIR}/rootfs/lib64/
+cp  ${SYSROOT}/lib64/libm*.so* ${OUTDIR}/rootfs/lib64/
+cp  ${SYSROOT}/lib64/libresolv*.so* ${OUTDIR}/rootfs/lib64/
+ln -sf ld*.so* ${OUTDIR}/rootfs/lib64/ld-linux-aarch64.so.1
 
 # TODO: Make device nodes
-
+echo "Creating device nodes..."
+sudo mknod -m 666 ${OUTDIR}/rootfs/dev/null c 1 3
+sudo mknod -m 600 ${OUTDIR}/rootfs/dev/console c 5 1
 # TODO: Clean and build the writer utility
-
+echo "Building writer utility..."
+cd ${FINDER_APP_DIR}
+make clean
+make CROSS_COMPILE=${CROSS_COMPILE}
 # TODO: Copy the finder related scripts and executables to the /home directory
 # on the target rootfs
+cd ${OUTDIR}/rootfs
+cp ${FINDER_APP_DIR}/finder.sh home
+cp ${FINDER_APP_DIR}/finder-test.sh home
+cp ${FINDER_APP_DIR}/writer home
+#mkdir home/conf
+cp ${FINDER_APP_DIR}/conf/assignment.txt home/conf 
+cp ${FINDER_APP_DIR}/conf/username.txt home/conf 
+cp ${FINDER_APP_DIR}/autorun-qemu.sh home
 
 # TODO: Chown the root directory
-
+#sudo chown -R root:root *
+#sudo chmod 777 home/finder.sh
 # TODO: Create initramfs.cpio.gz
+find . | cpio -H newc -ov --owner root:root > ${OUTDIR}/initramfs.cpio
+gzip -f ${OUTDIR}/initramfs.cpio
+
+echo "Build complete! Output in ${OUTDIR}"
